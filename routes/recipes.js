63624e7ce9fc57bca950019c4ea73eb07a9f0176
renad1999 <re0016@outlook.com>
2023-06-01 21:21:04 +0100

    OAuth working

---
+++ routes/recipes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// const recipesCtrl = require('../controllers/recipes');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
+
+module.exports = router;
